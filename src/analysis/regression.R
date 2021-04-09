603f71820cd6ca709cdbf72d44becaf3cc34e1bf
Bas Dekkers <bhcjdekkers@home.nl>
2021-04-09 14:43:58 +0200

    Improved labelnames of neighbourhood

--- src/analysis/regression.R
+++ src/analysis/regression.R
@@ -11,19 +11,19 @@ airbnb$date <- as.Date(airbnb$date)
 airbnb <- airbnb %>% 
             mutate(covid = ifelse(date >= "2020-03-01", 1, 0))
 
-airbnb <- dummy_cols(airbnb3, select_columns = "covid")
+airbnb <- dummy_cols(airbnb, select_columns = "covid")
 
 #creating a dummy for neighbourhood
-airbnb$neighbourhood <- gsub(" ", "", airbnb$neighbourhood)
-df$neighbourhood <- gsub("-", "_", df$neighbourhood)
 airbnb <- dummy_cols(airbnb, select_columns = "neighbourhood")
-
 
 #creating datasets for the linear analysis
 neighbourhood <- airbnb[10:31]
 covid <- airbnb[8:9]
 
 mdl_airbnb <- lm(num_reviews ~ month + covid + neighbourhood, data=airbnb)
+
+neigbourhood_label <- (colnames(neighbourhood))
+reg_name <- substr(neigbourhood_label, 15, nchar(neigbourhood_label))
 
 # Where to safe the outcome
 dir.create("../../gen")
@@ -35,7 +35,7 @@ stargazer(mdl_airbnb,
           title = "Influence of several factors on reviews in Amsterdam ",
           dep.var.caption = "Reviews",  
           dep.var.labels = "",  
-          covariate.labels = c("Month", "Covid"),  
+          covariate.labels = c("Month", "Covid", reg_name),  
           notes.label = "Significance levels",  
           type="html",
           out="../../gen/analysis/output/regression.html"  
